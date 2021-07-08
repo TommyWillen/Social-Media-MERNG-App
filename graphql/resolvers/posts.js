559aa15d257d0364fa28c1cbad5d47fc3e972a0f
Tommy Willen <tommyallen1215@gmail.com>
2021-07-08 09:50:05 -0700

    added model, built server and created most of the backend routes

---
+++ graphql/resolvers/posts.js
@@ -0,0 +1,119 @@
+const { AuthenticationError, UserInputError } = require("apollo-server");
+
+const Post = require("../../models/Post");
+const checkAuth = require("../../utilities/check-auth");
+
+module.exports = {
+  Query: {
+    async getPosts() {
+      try {
+        const posts = await Post.find().sort({ createdAt: -1 });
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+    async getPost(_, { postId }) {
+      try {
+        const post = await Post.findById(postId);
+        if (post) {
+          return post;
+        } else {
+          throw new Error("Post not found");
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+  Mutation: {
+    async createPost(_, { body }, context) {
+      const user = checkAuth(context);
+
+      const newPost = new Post({
+        body,
+        user: user.id,
+        username: user.username,
+        createdAt: new Date().toISOString(),
+      });
+
+      const post = await newPost.save();
+
+      return post;
+    },
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
+
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.username) {
+          await post.delete();
+          return "Post deleted successfully";
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+    async createComment(_, { postId, body }, context) {
+      const { username } = checkAuth(context);
+      if (body.trim() === "") {
+        throw new UserInputError("Empty Comment", {
+          errors: {
+            body: "Comment body must not be empty",
+          },
+        });
+      }
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+        post.comments.unshift({
+          body,
+          username,
+          createdAt: new Date().toISOString(),
+        });
+        await post.save();
+        return post;
+      } else {
+        throw new UserInputError("Post not found");
+      }
+    },
+    async deleteComment(_, { postId, commentId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+        const commentIndex = post.comments.findIndex((c) => c.id === commentId);
+        if (post.comments[commentIndex].username === username) {
+          post.comments.splice(commentIndex, 1);
+          await post.save();
+          return post;
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
+      } else {
+        throw new UserInputError("Post not found");
+      }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+        await post.save();
+        return post;
+      }
+    },
+  },
+};
---
+++ graphql/resolvers/users.js
@@ -0,0 +1,105 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { UserInputError } = require("apollo-server");
+
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../../utilities/validators");
+const { SECRET_KEY } = require("../../config");
+const User = require("../../models/User");
+
+const generateToken = (user) => {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+};
+
+module.exports = {
+  Mutation: {
+    async login(_, { username, password }) {
+      const { valid, errors } = validateLoginInput(username, password);
+      if (!valid) {
+        throw new UserInputError("Errors", {errors});
+      }
+
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        errors.general = "User not found";
+        throw new UserInputError("User not found", { errors });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = "Password is incorrect";
+        throw new UserInputError("Wrong Credentials", { errors });
+      }
+
+      const token = generateToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
+    async register(
+      _,
+      { registerInput: { username, email, password, confirmPassword } }
+    ) {
+      // validate user data
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+      // make sure user doesnt already exist
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new UserInputError("Username is taken", {
+          errors: {
+            username: "This username is taken",
+          },
+        });
+      }
+      const userEmail = await User.findOne({ email });
+      if (userEmail) {
+        throw new UserInputError("Username is taken", {
+          errors: {
+            email: "A username is already registered for this email",
+          },
+        });
+      }
+      // hash password and create an auth token
+      password = await bcrypt.hash(password, 12);
+
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+      });
+
+      const res = await newUser.save();
+
+      const token = generateToken(res);
+
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      };
+    },
+  },
+};
