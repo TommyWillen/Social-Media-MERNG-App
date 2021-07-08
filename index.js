bed11816f7e89a48ff2a2abd585298e1ee66a83d
Tommy Willen <tommyallen1215@gmail.com>
2021-07-08 11:57:32 -0700

    initial react front-end commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const server = new ApolloServer({
 });
 
 mongoose
-  .connect(MONGODB, { useNewUrlParser: true })
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     return server.listen({ port: 5000 });
   })
